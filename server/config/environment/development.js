63141ce2b9ed8dab4ef7e93896e96a184c6f774a
pavel.kogan <pavel.kogan@sizmek.com>
2016-12-17 18:32:56 +0200

    Update

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,8 +7,8 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    // uri: 'mongodb://pashaweb:pashak666@ds015325.mlab.com:15325/showbydate'
-    uri: 'mongodb://127.0.0.1:27017/showbydate'
+    uri: 'mongodb://pashaweb:pashak666@ds015325.mlab.com:15325/showbydate'
+    //uri: 'mongodb://127.0.0.1:27017/showbydate'
   },
 
   // Seed database on startup
